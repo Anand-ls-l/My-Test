13698c61d18e2b827c4d46b06d674b5d3467074e
Anand-ls-l <anandanil0206@gmail.com>
2024-10-05 18:46:12 +0000

    My Version 1 file

---
+++ aws_resource_tracker.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+set -x
+echo "Date of the day: " >> /home/user/Documents/Project1/output.txt
+date >> /home/user/Documents/Project1/output.txt
+echo "Details of S3 buckets created: " >> /home/user/Documents/Project1/output.txt
+aws s3 ls >> /home/user/Documents/Project1/output.txt
+echo "Details of EC2 instances created: " >> /home/user/Documents/Project1/output.txt
+aws ec2 describe-instances --query "Reservations[*].Instances[*].[InstanceId, KeyName, LaunchTime, State.Name]" --output table >> /home/user/Documents/Project1/output.txt
+echo "Details of lambda functons created: " >> /home/user/Documents/Project1/output.txt
+aws lambda list-functions >> /home/user/Documents/Project1/output.txt
+echo "Details of iam users: " >> /home/user/Documents/Project1/output.txt
+aws iam list-users >> /home/user/Documents/Project1/output.txt
